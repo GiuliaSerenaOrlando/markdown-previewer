9c7958c659e32634f6c75ef24038d74e7d7f0009
giuliaserenaorlando <giuliaserena.orlando@outlook.it>
2025-08-04 08:05:19 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import "./styles/App.scss"
+import { marked } from "marked"
+
+marked.setOptions({
+  breaks: true,
+})
+
+const defaultMarkdown = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Here is some inline code: \`<div></div>\`
+
+\`\`\`
+// This is a code block
+function sayHello(name) {
+  return "Hello, " + name;
+}
+\`\`\`
+
+You can also make text **bold**!  
+> Block quotes look like this.
+
+- List item 1
+- List item 2
+- List item 3
+
+[Visit freeCodeCamp](https://www.freecodecamp.org)
+
+![Image](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`
 
 function App() {
+  const [markdown, setMarkdown] = useState(defaultMarkdown)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="section">
+        <label htmlFor="editor">Markdown Editor</label>
+        <textarea
+          id="editor"
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        />
+      </div>
+
+      <div className="section">
+        <label htmlFor="preview">Preview</label>
+        <div
+          id="preview"
+          dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+        />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
